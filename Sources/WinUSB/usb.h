11e5fce530a1f1c96abfbd362050a32976a54d74
Alex Taffe <65547595+ataffeqeexo@users.noreply.github.com>
2021-06-14 14:12:23 -0400

    forward declare

--- Sources/WinUSB/usb.h
+++ Sources/WinUSB/usb.h
@@ -4,10 +4,7 @@
 #include <windows.h>
 #include <winnt.h>
 #include <winusb.h>
-enum _DEVPROPSTORE {
-    DEVPROP_STORE_SYSTEM,
-    DEVPROP_STORE_USER
-} DEVPROPSTORE, *PDEVPROPSTORE;
+typedef enum _DEVPROPSTORE _DEVPROPSTORE;
 #include <setupapi.h>
 
 #endif
